73d43a9ae16e392352052dc92483c1390fa0da8d
Spade1095 <henrym181@nycstudents.net>
2025-09-18 07:16:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-item = {
-    "name" : "henry",
-    "school": "Tech"
+students = [
+{
+    "name": "henry",
+    "school": "Tech",
+    "birthday": "07/15/11",
+},
+{
+    "name": "ayaan",
+    "school": "Tech",
+    "birthday":"06/27/11"
 }
+]
+
+for index, i in enumerate(students):
+    print(index, ":", i["name"],i["school"],i["birthday"])
